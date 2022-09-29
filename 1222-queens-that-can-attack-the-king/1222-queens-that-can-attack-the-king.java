5d4b01e5fecb22194f630578d0b5bbbbeb0b4a63
chitranshi135 <81587660+chitranshi135@users.noreply.github.com>
2022-09-29 11:32:15 +0530

    Time: 13 ms (5.04%), Space: 44.3 MB (5.04%) - LeetHub

--- 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.java
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.java
@@ -7,21 +7,19 @@ class Solution {
             String str = q[0] + "," + q[1];
             set.add(str);
         }
-        int x = king[0], y = king[1];
+        
         for(int i = -1; i <= 1; i++)
         {
             for(int j = -1; j <= 1; j++)
             {
                 if(i == 0 && j == 0)
                     continue;
-                for(int k = 1; k <= 8; k++)
-                {
-                    int a = x + (i*k);
-                    int b = y + (j*k);
-                    String s = a+","+b;
-                    if(set.contains(s))
-                    {
-                        list.add(Arrays.asList(a, b));
+                int x = king[0], y = king[1];
+                 while (x + i >= 0 && x + i < 8 && y + j >= 0 && y + j < 8) {
+                    x += i;
+                    y += j;
+                    if (set.contains(x+","+y)) {
+                        list.add(Arrays.asList(x, y));
                         break;
                     }
                 }
