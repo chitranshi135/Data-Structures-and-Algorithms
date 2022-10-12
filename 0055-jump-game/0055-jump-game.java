c87bd3501a018143a97a629cc4669e97f79531d0
chitranshi135 <81587660+chitranshi135@users.noreply.github.com>
2022-10-12 22:46:43 +0530

    Time: 3 ms (76.73%), Space: 67.6 MB (62.47%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -4,13 +4,13 @@ class Solution {
         int reach = 0;
         for(int i = 0; i < n; i++)
         {
-             if(reach >= n-1)
-                return true;
             if(i <= reach && (i+nums[i]) >= reach)
             {
                 reach = i+nums[i];
             }
         }
+        if(reach >= n-1)
+            return true;
         return false;
     }
 }
