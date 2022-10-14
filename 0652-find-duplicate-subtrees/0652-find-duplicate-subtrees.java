dd7eec6c186b1c61421d9391db50ec116ddd3805
chitranshi135 <81587660+chitranshi135@users.noreply.github.com>
2022-10-14 10:38:15 +0530

    Time: 38 ms (43.21%), Space: 63.6 MB (13.25%) - LeetHub

--- 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.java
+++ 0652-find-duplicate-subtrees/0652-find-duplicate-subtrees.java
@@ -15,27 +15,22 @@
  */
 class Solution {
     public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
-        Map<String, List<TreeNode>> map = new HashMap<String, List<TreeNode>>();
+        Map<String, Integer> map = new HashMap<String, Integer>();
         List<TreeNode> list = new LinkedList<>();
-        helper(root, map);
-        for (List<TreeNode> group : map.values())
-        {
-            if(group.size() > 1)
-                list.add(group.get(0));
-        }
+        helper(root, map, list);
         return list;
     }
-    public String helper(TreeNode root,  Map<String, List<TreeNode>> map)
+    public String helper(TreeNode root,  Map<String, Integer> map, List<TreeNode> list)
     {
         if(root == null)
             return "N";
         String sub = "";
-        String leftStr = helper(root.left, map);
-        String rightStr = helper(root.right, map);
+        String leftStr = helper(root.left, map, list);
+        String rightStr = helper(root.right, map, list);
         sub = "("+String.valueOf(root.val)+" "+leftStr+" "+rightStr+")";
-        if(!map.containsKey(sub))
-                map.put(sub, new ArrayList<TreeNode>());
-            map.get(sub).add(root);
+        map.put(sub, map.getOrDefault(sub, 0)+1);
+        if(map.get(sub) == 2)
+            list.add(root);
         return sub;
     }
 }
