e6d9d84a67aafbc711253924f7ba123ac8cbde49
chitranshi135 <81587660+chitranshi135@users.noreply.github.com>
2022-10-17 10:54:57 +0530

    Time: 2280 ms (18.29%), Space: 41.9 MB (53.18%) - LeetHub

---
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        int tot = 0;
+        for(int x : nums)
+            tot += x;
+        if(tot % k != 0)
+            return false;
+        int s = tot/k;
+        boolean[] vis = new boolean[nums.length];
+        Arrays.sort(nums);
+        return dfs(nums, 0, s, nums.length-1, vis, k);
+    }
+    public boolean dfs(int[] nums, int curr, int target, int start, boolean[] vis, int k)
+    {
+        if(k == 0)
+            return true;
+        //subset is completed before reaching this element
+        //so we start a new subset
+        if(curr == target && dfs(nums, 0, target, nums.length-1, vis, k-1))
+            return true;
+        for(int i = start; i >= 0; i--)
+        {
+            if(!vis[i] && nums[i]+curr <= target)
+            {
+                vis[i] = true;
+                if(dfs(nums, curr+nums[i], target, i-1, vis, k))
+                    return true;
+                vis[i] = false;
+            }
+        }
+        return false;
+    }
+}
