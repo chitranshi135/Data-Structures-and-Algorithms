4be40ddb9f077f8d6d0bfb64bfbd0d924122ab58
chitranshi135 <81587660+chitranshi135@users.noreply.github.com>
2022-12-02 11:12:44 +0530

    Time: 41 ms (45.78%), Space: 54.6 MB (66.67%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.java
@@ -5,6 +5,8 @@ class Solution {
         if(word1.length() != word2.length())
             return false;
         int n = word1.length();
+        // Order of characters doesn't matter because rule 1 allows reordering
+        //so we just need to observe frequencies
         for(int i = 0; i < n; i++)
         {
             char ch1 = word1.charAt(i);
@@ -15,6 +17,7 @@ class Solution {
         for(int i = 0; i < 26; i++)
         {
             //checking if the unique characters in both strings are same
+            //since no new chracaters can be intriduced in any word
             if((f1[i] > 0 && f2[i] == 0) || (f1[i] == 0 && f2[i] > 0))
             {
                 return false;
@@ -22,8 +25,9 @@ class Solution {
         }
         Arrays.sort(f1);
         Arrays.sort(f2);
+        //using the second operation, w ecan swap the frequencies of characters but no new frequency can be introduced
         //now we need to check that the exact same set of frequencies are present in both the words
-        //because when the swapping always happens in apirs
+        //because the swapping always happens in pairs
         for(int i = 0; i < 26; i++)
         {
             if(f1[i] != f2[i])
