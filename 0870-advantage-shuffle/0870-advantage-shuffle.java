f5439f283d703e4c39c46196379b08a4d8697eb0
chitranshi135 <81587660+chitranshi135@users.noreply.github.com>
2023-04-04 10:44:31 +0530

    Time: 232 ms (12.86%), Space: 61.5 MB (54.36%) - LeetHub

--- 0870-advantage-shuffle/0870-advantage-shuffle.java
+++ 0870-advantage-shuffle/0870-advantage-shuffle.java
@@ -1,28 +1,35 @@
 class Solution {
     public int[] advantageCount(int[] nums1, int[] nums2) {
         int n = nums1.length;
-        Arrays.sort(nums1);
+        TreeMap<Integer, Integer> map = new TreeMap<>();
         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->nums2[b]-nums2[a]);
-        for(int i = 0; i < nums2.length; i++)
+        for(int i = 0; i < n; i++)
         {
+            //for nums1
+            map.put(nums1[i], map.getOrDefault(nums1[i], 0)+1);
+            //for nums2
             pq.add(i);
         }
         int[] ans = new int[n];
         Arrays.fill(ans, -1);
-        int high = n-1;
-        int low = 0;
         while(!pq.isEmpty())
         {
             int idx = pq.poll();
-            if(nums1[high] > nums2[idx])
+            int val = map.lastKey();
+            if(val > nums2[idx])
             {
-                ans[idx] = nums1[high];
-                high--;
+                ans[idx] = val;
             }
             else
             {
-                ans[idx] = nums1[low++];
+                val = map.firstKey();
+                ans[idx] = val;
             }
+            int f = map.get(val);
+            if(f == 1)
+                map.remove(val);
+            else
+                map.put(val, f-1);
         }
         return ans;
     }
