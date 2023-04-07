c425d2b9e2c215f28888e6e4ffb87a1b85e20fea
chitranshi135 <81587660+chitranshi135@users.noreply.github.com>
2023-04-07 14:35:33 +0530

    Time: 18 ms (74.80%), Space: 43.9 MB (66.87%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.java
@@ -1,26 +1,25 @@
 class Solution {
     public int maxProfitAssignment(int[] d, int[] profit, int[] worker) {
         int p = 0;
+        Arrays.sort(worker);
         int n = profit.length;
         int[][] arr = new int[n][2];
         for(int i = 0; i < n; i++)
         {
-            arr[i][0] = profit[i];
-            arr[i][1] = d[i];
+            arr[i][0] = d[i];
+            arr[i][1] = profit[i];
         }
-        Arrays.sort(arr, (a,b)->b[0]-a[0]);
+        Arrays.sort(arr, (a,b)->a[0]-b[0]);
+        int i = 0;
+        int max = 0;
         for(int w : worker)
         {
-            int i = 0;
-            while(i < n)
+            while(i < n && arr[i][0] <= w)
             {
-                if(arr[i][1] <= w)
-                {
-                    p += arr[i][0];
-                    break;
-                }
+                max = Math.max(max,arr[i][1]);
                 i++;
             }
+            p += max;
         }
         return p;
     }
