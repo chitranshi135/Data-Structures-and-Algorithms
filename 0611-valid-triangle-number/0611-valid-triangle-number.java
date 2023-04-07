80390e8ab1dc86838d02722de2be16138afe1c98
chitranshi135 <81587660+chitranshi135@users.noreply.github.com>
2023-04-07 12:02:24 +0530

    Time: 36 ms (59.39%), Space: 42.7 MB (12.65%) - LeetHub

--- 0611-valid-triangle-number/0611-valid-triangle-number.java
+++ 0611-valid-triangle-number/0611-valid-triangle-number.java
@@ -4,12 +4,21 @@ class Solution {
         int n = nums.length;
         //we find pairs such that i < j < k
         int count = 0;
-        for (int i = 0; i < nums.length - 2; i++) {
-            for (int j = i + 1; j < nums.length - 1 && nums[i] != 0; j++) {
-                int k = j+1;
-                while (k < nums.length && nums[i] + nums[j] > nums[k])
-                    k++;
-                count += k - j - 1;
+        for (int k = 2; k < n; k++) {
+            int i = 0, j = k-1;
+            while(i < j)
+            {
+                if(nums[i]+nums[j] > nums[k])
+                {
+                    //fixed vale of j
+                    //range of i can be from i to j-1
+                    count += j-i;
+                    j--;
+                }
+                else
+                {
+                    i++;
+                }
             }
         }
         return count;
