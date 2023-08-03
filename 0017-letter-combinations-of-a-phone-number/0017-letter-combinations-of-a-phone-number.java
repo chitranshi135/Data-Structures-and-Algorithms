d39cc2d5287712ad116fc2f1638d715462957b44
Chitranshi Srivastava <81587660+chitranshi135@users.noreply.github.com>
2023-08-03 14:22:29 +0530

    Time: 5 ms (50.30%), Space: 41.2 MB (64.44%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -3,23 +3,24 @@ class Solution {
         
         if(digits.length() == 0)
             return new ArrayList<>();
-        String[] comb = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        String[] comb = new String[] {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         List<String> ans = new ArrayList<>();
         
-        combine(comb, "", digits.toCharArray(), 0, ans);
+        combine(comb, "", digits.toCharArray(), ans);
         return ans;
     }
     
-    public void combine(String[] comb, String prefix, char[] digits, int idx, List<String> ans) {
-        if(idx == digits.length)
+    public void combine(String[] comb, String prefix, char[] digits, List<String> ans) {
+        if(prefix.length() == digits.length)
         {
             ans.add(prefix);
             return;
         }
+        int idx = prefix.length();
         String chars = comb[digits[idx]-'0'];
         for(char ch : chars.toCharArray())
         {
-            combine(comb, prefix+ch, digits, idx+1, ans);
+            combine(comb, prefix+ch, digits, ans);
         }
     }
 
