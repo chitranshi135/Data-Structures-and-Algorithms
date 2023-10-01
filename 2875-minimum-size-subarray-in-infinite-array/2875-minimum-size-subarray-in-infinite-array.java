ca12a1f25febd70fbe7bf81c3162d8b472d159b7
Chitranshi Srivastava <81587660+chitranshi135@users.noreply.github.com>
2023-10-01 09:49:08 +0530

    Time: 6 ms (50.00%), Space: 53.5 MB (50.00%) - LeetHub

---
+++ 2875-minimum-size-subarray-in-infinite-array/2875-minimum-size-subarray-in-infinite-array.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int minSizeSubarray(int[] nums, int target) {
+        int n = nums.length;
+        long tot = 0;
+        for(int i = 0; i < n; i++)
+            tot += nums[i];
+        int ans = 0;
+        if(target > (long)tot)
+        {
+            ans = (int)(target/tot) * n;
+            target = (int)(target % tot);
+        }
+        int i = 0, j = 0;
+        int min = Integer.MAX_VALUE;
+        int l = 2*n;
+        long sum = 0;
+        while(j < l)
+        {
+            while(j < l && sum + nums[j%n] <= target)
+            {
+                sum += nums[j%n];
+                j++;
+            }
+            //System.out.println(i+" "+j+" "+sum);
+            if(sum == target)
+                min = Math.min(min, j-i);
+            
+            sum -= nums[i%n];
+            i++; 
+        }
+        return (min == Integer.MAX_VALUE) ? -1 : min+ans;
+    }
+}
