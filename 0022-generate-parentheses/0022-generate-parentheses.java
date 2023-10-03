8ed85d7ac09e6cdedc6f6e156549c41dcb2edbc8
Chitranshi Srivastava <81587660+chitranshi135@users.noreply.github.com>
2023-10-03 10:00:53 +0530

    Time: 10 ms (7.06%), Space: 44.2 MB (6.40%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,51 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        int[] brackets = {n, n};
+        solve(brackets, "", ans, n);
+        return ans;
+    }
+    
+    public boolean isValid(String str)
+    {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < str.length(); i++)
+        {
+            char ch = str.charAt(i);
+            if(ch == '(')
+                stack.push('(');
+            else
+            {
+                if(stack.size() > 0)
+                    stack.pop();
+            }
+        }
+        return stack.size() == 0;
+    }
+    public void solve(int[] brackets, String curr, List<String> ans, int n)
+    {
+        if(curr.length() == 2*n)
+        {
+            if(isValid(curr))
+                ans.add(curr);
+            return;
+        }
+        
+        for(int i = 0; i < 2; i++)
+        {
+            if(brackets[i] > 0)
+            {
+                brackets[i]--;
+                if(i == 0)
+                {
+                    solve(brackets, curr+"(", ans, n);
+                }
+                else
+                {
+                    solve(brackets, curr+")", ans, n);
+                }
+                brackets[i]++;
+            }
+        }
+    }
+}
