99d3a90ea3372181947d44963880e522d2f4fcd7
Chitranshi Srivastava <81587660+chitranshi135@users.noreply.github.com>
2023-10-04 15:08:29 +0530

    Time: 49 ms (66.42%), Space: 39.6 MB (70.19%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
@@ -2,16 +2,13 @@ class Solution {
     public int distributeCookies(int[] cookies, int k) {
         int[] curr = new int[k];
         int[] res = {Integer.MAX_VALUE};
-        solve(0, cookies, curr, res);
+        solve(0, cookies, curr, res, 0);
         return res[0];
     }
     
-    public void solve(int idx, int[] cookies, int[] curr, int[] res) {
+    public void solve(int idx, int[] cookies, int[] curr, int[] res, int max) {
         if(idx == cookies.length)
         {
-            int max = -1;
-            for(int x: curr)
-                max = Math.max(max, x);
             res[0] = Math.min(res[0], max);
             return;
         }
@@ -20,7 +17,7 @@ class Solution {
         {
             curr[i] += cookies[idx];
             if(curr[i] < res[0])
-                solve(idx+1, cookies, curr, res);
+                solve(idx+1, cookies, curr, res, Math.max(max, curr[i]));
             curr[i] -= cookies[idx];
         }
     }
