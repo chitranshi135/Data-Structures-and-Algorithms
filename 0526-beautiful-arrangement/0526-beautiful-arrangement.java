e34c6b3aa4bd893f51ab20ec016cbf4802869af7
Chitranshi Srivastava <81587660+chitranshi135@users.noreply.github.com>
2023-10-05 22:33:18 +0530

    Time: 41 ms (52.73%), Space: 38.6 MB (96.86%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.java
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.java
@@ -1,24 +1,18 @@
-class Solution {
-    public int countArrangement(int n) {
-        int[] count = new int[1];
-        solve(1, n, new ArrayList<Integer>(), count);
-        return count[0];
+public class Solution {
+    int count = 0;
+    public int countArrangement(int N) {
+        boolean[] visited = new boolean[N + 1];
+        calculate(N, 1, visited);
+        return count;
     }
-    
-    public void solve(int idx, int n, List<Integer> list, int[] count) {
-        if(idx > n)
-        {
-            count[0]++;
-            return;
-        }
-        
-        for(int i = 1; i <= n; i++) {
-            if(!list.contains(i)){
-                if(idx % i == 0 || i % idx == 0){
-                    list.add(i);
-                    solve(idx+1, n, list, count);
-                    list.remove(list.size()-1);
-                }
+    public void calculate(int N, int pos, boolean[] visited) {
+        if (pos > N)
+            count++;
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i] && (pos % i == 0 || i % pos == 0)) {
+                visited[i] = true;
+                calculate(N, pos + 1, visited);
+                visited[i] = false;
             }
         }
     }
