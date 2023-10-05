7561f5d437d614aa1a246d57ce1a19409b1b0b92
Chitranshi Srivastava <81587660+chitranshi135@users.noreply.github.com>
2023-10-05 15:08:15 +0530

    Time: 2 ms (98.52%), Space: 47 MB (22.48%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,19 +1,44 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         int n = nums.length;
+        //Moore's Voting Algorithm
+        //There can be a maximum of 2 elements
         List<Integer> ans = new ArrayList<>();
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < n; i++)
-        {
-            map.put(nums[i], map.getOrDefault(nums[i], 0)+1);
-        }
+        int ele1 = Integer.MAX_VALUE;
+        int ele2 = Integer.MAX_VALUE;
+        int count1 = 0;
+        int count2 = 0;
         
-        for(int x: map.keySet())
-        {
-            int val = map.get(x);
-            if(val > n/3)
-                ans.add(x);
+        for(int i = 0; i < n; i++){
+            if(count1 == 0 && nums[i] != ele2){
+                ele1 = nums[i];
+                count1 = 1;
+            }
+            else if(count2 == 0 && nums[i] != ele1){
+                ele2 = nums[i];
+                count2 = 1;
+            }
+            else if(nums[i] == ele1)
+                count1++;
+            else if(nums[i] == ele2)
+                count2++;
+            else{
+                count1--;
+                count2--;
+            }
         }
+        count1 = count2 = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == ele1)
+                count1++;
+            else if(nums[i] == ele2)
+                count2++;
+        }
+        if(count1 > n/3)
+            ans.add(ele1);
+        if(count2 > n/3)
+            ans.add(ele2);
+        
         return ans;
     }
 }
