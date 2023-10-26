931c69ead330f91fbb9fa3ac0a4290fbec5f382a
Chitranshi Srivastava <81587660+chitranshi135@users.noreply.github.com>
2023-10-26 13:52:30 +0530

    Added solution - LeetHub

---
+++ Possible paths between 2 vertices - GFG/possible-paths-between-2-vertices.java
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                // adj.get(v).add(u);
+            }
+            String[] S1 = br.readLine().trim().split(" ");
+            int source = Integer.parseInt(S1[0]);
+            int destination = Integer.parseInt(S1[1]);
+            Solution obj = new Solution();
+            System.out.println(obj.countPaths(V, adj, source, destination));
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    
+    public void helper(int source, int destination, ArrayList<ArrayList<Integer>> adj, int[] count) {
+        if(source == destination){
+            count[0]++;
+            return;
+        }
+        for(int x: adj.get(source)){
+            helper(x, destination, adj, count);
+        }
+    }
+    // Function to count paths between two vertices in a directed graph.
+    public int countPaths(int V, ArrayList<ArrayList<Integer>> adj, int source,
+                   int destination) {
+        int[] count = {0};
+        helper(source, destination, adj, count);
+        return count[0];
+    }
+}
