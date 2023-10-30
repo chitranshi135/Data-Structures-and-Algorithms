c5560b553dfddd6a23abdeda83d85a3d405db1b2
Chitranshi Srivastava <81587660+chitranshi135@users.noreply.github.com>
2023-10-30 18:35:21 +0530

    Time: 7 ms (61.31%), Space: 43.2 MB (97.93%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -1,15 +1,27 @@
 class Solution {
     public int[] sortByBits(int[] arr) {
-        int[][] a = new int[arr.length][2];
-        for(int i = 0; i < arr.length; i++){
-            a[i][0] = arr[i];
-            a[i][1] = Integer.bitCount(arr[i]);
-        }
-        Arrays.sort(a, (x, y) -> (x[1]==y[1]) ? x[0]-y[0] : x[1]-y[1]);
+        Integer[] a = new Integer[arr.length];
+        for(int i = 0; i < arr.length; i++)
+            a[i] = arr[i];
+        
+        Arrays.sort(a, new CustomCompare());
         
         for(int i = 0; i < arr.length; i++){
-            arr[i] = a[i][0];
+            arr[i] = a[i];
         }
         return arr;
+    }
+}
+
+class CustomCompare implements Comparator<Integer> {
+    @Override
+    public int compare(Integer a, Integer b) {
+        int c1 = Integer.bitCount(a);
+        int c2 = Integer.bitCount(b);
+        
+        if(c1 == c2)
+            return a-b;
+        
+        return c1-c2;
     }
 }
