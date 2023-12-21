4b502b00eea73fb769eb4371f715b585f3b0fdaf
Chitranshi Srivastava <81587660+chitranshi135@users.noreply.github.com>
2023-12-21 10:17:21 +0530

    Time: 5 ms (83.29%), Space: 45.3 MB (6.68%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.java
@@ -8,6 +8,8 @@ class Solution {
             for(int j=0;j<n;j++){
                 fr[matrix[i][j]]++;
                 fc[matrix[j][i]]++;
+                if(fr[matrix[i][j]] > 1 || fc[matrix[j][i]] > 1)
+                    return false;
             }
             
             for(int s=1;s<=n;s++){
